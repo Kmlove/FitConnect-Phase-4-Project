aeaac712d30cd779cee0b81e79029a4752014513
Kim Love <kimberlymlove15@gmail.com>
2023-10-17 12:18:44 -0400

    Created components folder and added new components

---
+++ client/src/components/Profile.js
@@ -0,0 +1,11 @@
+function Profile({name, imageURL}){
+    console.log(name)
+    return (
+        <>
+            <img src="" alt=""/>
+            <h3>{}</h3>
+        </>
+    )
+}
+
+export default Profile;
